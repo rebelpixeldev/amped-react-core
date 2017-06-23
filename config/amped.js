f322061b54dc49bdd8549c8b9c59113e33a94a19
rebelpixeldev <tsczelecki@konradgroup.com>
2017-06-23 10:20:06 -0400

    Trying another npm boilerplate

--- config/amped.js
+++
@@ -1,3 +0,0 @@
-const env = process.env.NODE_ENV || 'local'
-
-module.exports = Object.assign(require('./env/all'), require(`./env/${env}`));
--- config/env/all.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	site : {
-		name : 'Amped Admin'
-	}
-}
